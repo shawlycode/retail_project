ebd01ca7c03cdca7f62fa569b72782ed89c4265f
Shawlycode <kutinaorlando@gmail.com>
2022-02-26 12:29:30 +0000

    craeted components

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            Home Page
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Home;
