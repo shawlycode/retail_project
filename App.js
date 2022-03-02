9106001680884ffdc549cf04aefb804fa2fd136c
Shawlycode <kutinaorlando@gmail.com>
2022-03-02 11:08:10 +0000

    rendered router

--- App.js
+++ App.js
@@ -1,28 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './src/Components/Home';
+import React from "react";
+import { View, Text } from "react-native";
+import Route from "./src/Navigation/Route";
 
+const App = () => {
+  return <Route />;
+};
 
-
-
-const Stack = createNativeStackNavigator();
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='home' component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
