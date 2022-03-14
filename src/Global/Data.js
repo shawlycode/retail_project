c884459cd87bce5c231d8fbb997d46c01ba57e6b
Shawlycode <kutinaorlando@gmail.com>
2022-03-14 21:48:43 +0000

    added data component

---
+++ src/Global/Data.js
@@ -0,0 +1,140 @@
+export const COLOURS = {
+  white: "#ffffff",
+  dark: "#023047",
+  green: "#80ed99",
+  red: "#C04345",
+  background: "#ade8f4",
+  backgroundMedium: "#B9B9B9",
+};
+export const categoryData = [
+  {
+    id: "8387",
+    name: "DRY FOODS",
+    icon: require("../../assets/images/category_img/dried-fruits.png"),
+  },
+  {
+    id: "sddg",
+    name: "FROZEN FOODS",
+    icon: require("../../assets/images/category_img/frozen-yogurt.png"),
+  },
+  {
+    id: "sddscsfsfg",
+    name: "BAKES & SNACKS",
+    icon: require("../../assets/images/category_img/cookie.png"),
+  },
+  {
+    id: "sddg-667",
+    name: "GROCERY",
+    icon: require("../../assets/images/category_img/groceries.png"),
+  },
+  {
+    id: "sddg-duy",
+    name: "TOILETRIES",
+    icon: require("../../assets/images/category_img/toiletries.png"),
+  },
+  {
+    id: "sddg-ggduy",
+    name: "BABY CARE",
+    icon: require("../../assets/images/category_img/milk-bottle.png"),
+  },
+  {
+    id: "sdd33g",
+    name: "STATIONERY",
+    icon: require("../../assets/images/category_img/stationery.png"),
+  },
+  {
+    id: "s122ddg",
+    name: "BEVERAGES",
+    icon: require("../../assets/images/category_img/beverage.png"),
+  },
+  {
+    id: "sdd-tte6g",
+    name: "HEALTH CARE",
+    icon: require("../../assets/images/category_img/care.png"),
+  },
+];
+export const products_Category = [
+  {
+    id: "hhgs-d6",
+    name: "FAN YOGURT",
+    price: "40",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    trending: true,
+    products_img: require("../../assets/images/products/yogurt.png"),
+    category: ["FROZEN FOODS"]
+  },
+  {
+    id: "hhg6",
+    name: "SHORT BREAD",
+    price: "40",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    trending: true,
+    products_img: require("../../assets/images/products/short.webp"),
+    category: ["BAKES & SNACKS"]
+  },
+  {
+    id: "hhgs43d6",
+    name: "NESCAFEE",
+    price: "40",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    trending: true,
+    products_img: require("../../assets/images/products/cafeee.jpeg"),
+    category: ["GROCERY"]
+  },
+  {
+    id: "hhg-222sd6",
+    name: "RICE",
+    price: "40",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    trending: true,
+    products_img: require("../../assets/images/products/rice.jpeg"),
+    category: ["DRY FOODS"]
+  },
+
+  {
+    id: "hhegsd6",
+    name: "INDOMIE",
+    price: "40",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    trending: true,
+    products_img: require("../../assets/images/products/yogurt.png"),
+    category: ["DRY FOODS"]
+  },
+
+  {
+    id: "hhgqq-sd6",
+    name: "SANITARY PAD",
+    price: "40",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    trending: true,
+    products_img: require("../../assets/images/products/yogurt.png"),
+    category: ["HEALTH CARE"]
+  },
+  {
+    id: "hhgsqwed6",
+    name: "MILK",
+    price: "40",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    trending: true,
+    products_img: require("../../assets/images/products/milk.jpg"),
+    category: ["BABY CARE"]
+  },
+
+  {
+    id: "hhg98sd6",
+    name: "PASTE",
+    price: "40",
+    description:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    trending: true,
+    products_img: require("../../assets/images/products/paste.jpeg"),
+    category: ["HEALTH CARE"]
+  },
+];
